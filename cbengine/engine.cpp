45d5b32930dd499816b9082fc27ad388e50794f1
bhhwang <bhhwang@nsynapse.com>
2015-06-09 20:30:00 +0900

    Firtst Commit

---
+++ cbengine/engine.cpp
@@ -0,0 +1,14 @@
+/*
+ * COSSB Engine
+ *
+ * Author : Byunghun Hwang<bhhwang@nsynapse.com>
+ * Date : 2015. 6. 9
+ *
+ */
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
