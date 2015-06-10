14909a65500b0b51c1f84550874541380b068b7f
elecun <elecun2@gmail.com>
2015-06-10 10:36:12 +0900

    add cossb header file

---
+++ include/cossb.hpp
@@ -0,0 +1,19 @@
+/**
+ * @file		cossb.hpp
+ * @brief		COSSB Header
+ * @author		Byunghun Hwang<bhhwang@nsynapse.com>
+ * @date 		2015. 6. 9
+ * @details
+ */
+
+#ifndef _COSSB_HEADER_HPP_
+#define _COSSB_HEADER_HPP_
+
+#if defined(__unix__) || defined(__gnu_linux__) || defined(linux) || defined(__linux__)
+
+
+#include "version.hpp"
+
+#endif
+
+#endif
