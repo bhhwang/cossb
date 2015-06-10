39540e77b0b73e10b76b97e72bb59e343a7e11b9
elecun <elecun2@gmail.com>
2015-06-10 10:36:35 +0900

    add interrupt signal

--- cbengine/engine.cpp
+++ cbengine/engine.cpp
@@ -13,8 +13,20 @@
  */
 
 #include <iostream>
+#include <csignal>
+#include "cossb.hpp"
 
+using namespace std;
+
+void sigc_interrupt(int s) { exit(0); }
+
+/**
+ * @brief	Main routine
+ * @param	command
+ * @details	Start with default components
+ */
 int main(int argc, char* argv[])
 {
+	signal(SIGINT, sigc_interrupt);
 	return 0;
 }
