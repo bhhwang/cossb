143bbcf13068a9e848ad0d7cc08d70181d65c8d4
elecun <elecun2@gmail.com>
2015-06-12 17:49:27 +0900

    add log header file for own log interface

---
+++ include/log.hpp
@@ -0,0 +1,15 @@
+/**
+ * @file		log.hpp
+ * @brief		Logger
+ * @author		Byunghun Hwang<bhhwang@nsynapse.com>
+ * @date 		2015. 6. 12
+ * @details	logger using spdlog library
+ */
+
+#ifndef _COSSB_LOG_HPP_
+#define _COSSB_LOG_HPP_
+
+
+#include "./spdlog/spdlog.h"
+
+#endif
