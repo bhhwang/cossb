afc5e94c14591a832c5088666a2ee0199a47aa7a
elecun <elecun2@gmail.com>
2015-06-21 20:34:17 +0900

    add empty component driver class

---
+++ include/driver.cpp
@@ -0,0 +1,7 @@
+/**
+ * @file		driver.cpp
+ * @brief		Component driver
+ * @author		Byunghun Hwang<bhhwang@nsynapse.com>
+ * @date 		2015. 6. 21
+ * @details	Load components with driver
+ */
---
+++ include/driver.hpp
@@ -0,0 +1,7 @@
+/**
+ * @file		driver.hpp
+ * @brief		Component driver
+ * @author		Byunghun Hwang<bhhwang@nsynapse.com>
+ * @date 		2015. 6. 21
+ * @details	Load components with driver
+ */
