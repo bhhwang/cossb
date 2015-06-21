67713bbb595d7b3743dc1be77cf21923ce2d23fe
elecun <elecun2@gmail.com>
2015-06-21 20:35:20 +0900

    add typedef header

---
+++ include/typedef.hpp
@@ -0,0 +1,42 @@
+/**
+ * @file		typedef.hpp
+ * @brief		COSSB Type definitions
+ * @author		Byunghun Hwang<bhhwang@nsynapse.com>
+ * @date 		2015. 6. 21
+ * @details
+ */
+
+#ifndef _COSSB_TYPEDEF_HPP_
+#define _COSSB_TYPEDEF_HPP_
+
+
+#include <boost/thread.hpp>
+#include <boost/shared_ptr.hpp>
+
+using namespace boost;
+
+namespace coosb {
+
+namespace process {
+	typedef boost::shared_ptr<boost::thread> task;
+
+	#define task_register(fnptr)	boost::thread(boost::bind(fnptr, this))
+	#define create_task(fnptr)	boost::shared_ptr<boost::thread>(new task_register(&fnptr))
+	#define destroy_task(instance)	if(instance){ instance->interrupt(); instance->join(); }
+}
+
+namespace types {
+
+	#define COMPONENT_EXT	".comp"
+	#define PROFILE_EXT		".xml"
+
+	enum class comp_type : unsigned int { ASSIST=101, SERVICE, APP, GUI };
+	enum class comp_status : unsigned int { READY = 0, RUNNING, STOPPED };
+	enum class return_type : int { EXIST=-1, FAIL=0, SUCCESS=1 };
+
+
+} /* namespace types */
+} /* namespace cossb */
+
+
+#endif /* _COSSB_TYPEDEF_HPP_ */
