e15e85e92d04dd4d0add26f830af90602691ba1a
elecun <elecun2@gmail.com>
2015-06-21 20:33:17 +0900

    add empty base component interface class

---
+++ include/interface/icomponent_base.hpp
@@ -0,0 +1,14 @@
+/**
+ * @file		icomponent_base.hpp
+ * @brief		Component(Shared Library) base interface
+ * @author		Byunghun Hwang<bhhwang@nsynapse.com>
+ * @date 		2015. 6. 21
+ * @details	Component base interface class
+ */
+
+#ifndef _COSSB_ICOMPONENT_BASE_HPP_
+#define _COSSB_ICOMPONENT_BASE_HPP_
+
+
+
+#endif /* _COSSB_ICOMPONENT_BASE_HPP_ */
