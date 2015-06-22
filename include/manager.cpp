408aac862c178e4d8db5659d1c63d5a4ba871cb4
elecun <elecun2@gmail.com>
2015-06-22 15:32:18 +0900

    add manager functions

--- include/manager.cpp
+++ include/manager.cpp
@@ -5,3 +5,26 @@
  * @date 		2015. 6. 21
  * @details
  */
+
+#include "manager.hpp"
+
+namespace cossb {
+namespace manager {
+
+component_manager* component_manager::_instance = nullptr;
+
+component_manager* component_manager::get()
+{
+	if(!_instance)
+		_instance = new component_manager;
+	return _instance;
+}
+
+void component_manager::destroy()
+{
+	if(_instance)
+		delete _instance;
+}
+
+} /* namespace manager */
+} /* namespace cossb */
