8527c8a8d9eb56a8e8693420e252db660746cf31
elecun <elecun2@gmail.com>
2015-06-22 13:37:19 +0900

    defined simple component base interface

--- include/interface/icomponent.hpp
+++ include/interface/icomponent.hpp
@@ -9,14 +9,57 @@
 #ifndef _COSSB_ICOMPONENT_HPP_
 #define _COSSB_ICOMPONENT_HPP_
 
+#include <string>
+#include "../typedef.hpp"
+
+using namespace std;
+
 namespace cossb {
 namespace interface {
 
+/**
+* @brief		component interface class
+* @details		base component interface, lifecycle is (setup-run-stop)
+* @author		Byunghun Hwang<bhhwang@nsynapse.com>
+* @date		2015. 6. 22
+*/
 class icomponent {
+public:
+	virtual ~icomponent() = 0;
+
+	/**
+	 * @brief		initialization
+	 * @return		true if it is success
+	 */
+	virtual bool setup() = 0;
+
+	/**
+	 * @brief		work process logic (loop)
+	 */
+	virtual bool run() = 0;
+
+	/**
+	 * @brief
+	 */
+	const char* get_name() { return _name.c_str(); }
+
+
+protected:
+	explicit icomponent(const char* name):_name(name) {
+
+	}
+
+private:
+	string _name;
 
 };
 
 } /* namespace interface */
+
+
+typedef interface::icomponent*(*create_component)(void);
+typedef void(*destroy_component)(void);
+
 } /* namespace cossb */
 
 
