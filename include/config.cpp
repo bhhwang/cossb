75c535998966dbfb2210a90d75574da7e08f7fb3
elecun <elecun2@gmail.com>
2015-06-22 15:30:43 +0900

    add config file to read manifest file

---
+++ include/config.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file		config.CPP
+ * @brief		Config file loader
+ * @author		Byunghun Hwang<bhhwang@nsynapse.com>
+ * @date 		2015. 6. 21
+ * @details	Load config file to run the engine
+ */
+
+#include "config.hpp"
+
+namespace cossb {
+
+config::config() {
+	// TODO Auto-generated constructor stub
+
+}
+
+config::~config() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace cossb */
---
+++ include/config.hpp
@@ -0,0 +1,22 @@
+/**
+ * @file		config.hpp
+ * @brief		Config file loader
+ * @author		Byunghun Hwang<bhhwang@nsynapse.com>
+ * @date 		2015. 6. 21
+ * @details	Load config file to run the engine
+ */
+
+#ifndef _COSSB_CONFIG_HPP_
+#define _COSSB_CONFIG_HPP_
+
+namespace cossb {
+
+class config {
+public:
+	config();
+	virtual ~config();
+};
+
+} /* namespace cossb */
+
+#endif /* _COSSB_CONFIG_HPP_ */
