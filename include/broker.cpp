81371b314d8d619c893b7f624918cbfb736d94bb
elecun <elecun2@gmail.com>
2015-06-22 15:31:49 +0900

    add interfaces

--- include/broker.cpp
+++ include/broker.cpp
@@ -5,3 +5,36 @@
  * @date 		2015. 6. 21
  * @details
  */
+
+#include "broker.hpp"
+
+namespace cossb {
+namespace broker {
+
+component_broker* component_broker::_instance = nullptr;
+component_broker* component_broker::get()
+{
+	if(!_instance)
+		_instance = new component_broker;
+	return _instance;
+}
+
+void component_broker::destroy()
+{
+	if(_instance)
+		delete _instance;
+}
+
+bool component_broker::regist(interface::icomponent* component, topic topic_name)
+{
+	bool success = false;
+	if(_instance) {
+		_topic_map.insert(topic_map::value_type(topic_name, component->get_name()));
+		success = true;
+	}
+
+	return success;
+}
+
+} /* namespace broker */
+} /* namespace cossb */
--- include/broker.hpp
+++ include/broker.hpp
@@ -5,3 +5,39 @@
  * @date 		2015. 6. 21
  * @details
  */
+
+#ifndef _COSSB_BROKER_HPP_
+#define _COSSB_BROKER_HPP_
+
+#include <map>
+#include <string>
+#include "interface/icomponent.hpp"
+
+using namespace std;
+
+namespace cossb {
+namespace broker {
+
+typedef string topic;
+typedef string comp_name;
+typedef multimap<topic, comp_name> topic_map;
+
+class component_broker {
+public:
+	static component_broker* get();
+	void destroy();
+
+	/**
+	 *@brief	regist component with topic
+	 */
+	bool regist(interface::icomponent* component, topic topic_name);
+
+private:
+	static component_broker* _instance;
+	topic_map	_topic_map;
+};
+
+} /* namespace broker */
+} /* namespace cossb */
+
+#endif
