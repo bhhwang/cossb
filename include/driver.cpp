03f4f962022956e9dffb2977b81b911e0351856a
elecun <elecun2@gmail.com>
2015-06-23 10:07:45 +0900

    add component driver class

--- include/driver.cpp
+++ include/driver.cpp
@@ -5,3 +5,65 @@
  * @date 		2015. 6. 21
  * @details	Load components with driver
  */
+
+#include "driver.hpp"
+#include <dlfcn.h>
+#include "util/spdlog/spdlog.h"
+
+namespace cossb {
+namespace driver {
+
+component_driver::component_driver(const char* component_name)
+:_component_name(component_name)
+{
+	if(load(component_name))
+	{
+
+	}
+	else
+		spdlog::get(_component_name)->error("Cannot load {}", component_name);
+}
+
+component_driver::~component_driver()
+{
+
+}
+
+bool component_driver::load(const char* component_name)
+{
+	string comp_path = "./"+string(component_name);
+
+	_handle = dlopen(comp_path.c_str(), RTLD_LAZY|RTLD_GLOBAL);
+
+	if(!_handle)
+	{
+		spdlog::get("main_thread")->error("{}", dlerror());
+		return false;
+	}
+	else
+	{
+		create_component pfcreate = (create_component)dlsym(_handle, "create");
+		if(!pfcreate)
+		{
+			spdlog::get("main_thread")->error("{}",dlerror());
+
+			dlclose(_handle);
+			_handle = nullptr;
+
+			return false;
+		}
+		else
+		{
+			_ptr_component = pfcreate();
+			return true;
+		}
+	}
+}
+
+void component_driver::unload()
+{
+
+}
+
+} /* namespace dirver */
+} /* namespace cossb */
--- include/driver.hpp
+++ include/driver.hpp
@@ -5,3 +5,35 @@
  * @date 		2015. 6. 21
  * @details	Load components with driver
  */
+
+#ifndef _COSSB_DRIVER_HPP_
+#define _COSSB_DRIVER_HPP_
+
+#include <string>
+
+using namespace std;
+
+namespace cossb {
+namespace driver {
+
+class component_driver {
+public:
+	component_driver(const char* component_name);
+	virtual ~component_driver();
+
+	interface::icomponent* get_component() const;
+
+private:
+	bool load(const char* component_name);
+	void unload();
+
+private:
+	interface::icomponent* _ptr_component = nullptr;
+	void* _handle = nullptr;
+	string _component_name;
+};
+
+} /* namespace driver */
+} /* namespace cossb */
+
+#endif
