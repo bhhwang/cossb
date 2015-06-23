c1493b9701d2b001c7cfe4171d26b4bf0f813c5e
elecun <elecun2@gmail.com>
2015-06-23 10:10:21 +0900

    add log header

--- include/log.hpp
+++ include/log.hpp
@@ -1,13 +1,31 @@
 /**
  * @file		log.hpp
- * @brief		Logger
+ * @brief		log handler
  * @author		Byunghun Hwang<bhhwang@nsynapse.com>
- * @date 		2015. 6. 12
- * @details	logger using spdlog library
+ * @date 		2015. 6. 21
+ * @details	log handler
  */
 
 #ifndef _COSSB_LOG_HPP_
 #define _COSSB_LOG_HPP_
 
+#include <string>
 
-#endif
+using namespace std;
+
+namespace cossb {
+
+class log : public ilog {
+
+public:
+	static log* get();
+	void destroy();
+
+private:
+	static log* _instance;
+
+};
+
+} /* namespace cossb */
+
+#endif /* _COSSB_LOG_HPP_ */
