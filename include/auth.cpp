6d3cbe5613ac62fda88c5a3bf0d2ecc16d58b11d
elecun <elecun2@gmail.com>
2015-08-01 21:54:45 +0900

    first commit, authentication class for component download

---
+++ include/auth.cpp
@@ -0,0 +1,21 @@
+/*
+ * auth.cpp
+ *
+ *  Created on: 2015. 7. 29.
+ *      Author: hwang-linux
+ */
+
+#include "auth.hpp"
+
+namespace cossb {
+
+auth::auth() {
+	// TODO Auto-generated constructor stub
+
+}
+
+auth::~auth() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace cossb */
---
+++ include/auth.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file		auth.hpp
+ * @brief		Authentication module
+ * @author		Byunghun Hwang<bhhwang@nsynapse.com>
+ * @date 		2015. 7. 29
+ * @details	Authentication for service component download, service license..
+ */
+
+#ifndef INCLUDE_AUTH_HPP_
+#define INCLUDE_AUTH_HPP_
+
+namespace cossb {
+namespace auth {
+
+class auth {
+public:
+	auth();
+	virtual ~auth();
+};
+
+} /* namesoace auth */
+} /* namespace cossb */
+
+#endif /* INCLUDE_AUTH_HPP_ */
