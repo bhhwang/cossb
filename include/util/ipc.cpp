8cb3797d918a5606732bdbd6c58ad922075ea66a
elecun <elecun2@gmail.com>
2015-08-01 21:57:17 +0900

    first commit, add internal process communication class

---
+++ include/util/ipc.cpp
@@ -0,0 +1,21 @@
+/*
+ * ipc.cpp
+ *
+ *  Created on: 2015. 8. 1.
+ *      Author: hwang-linux
+ */
+
+#include "ipc.hpp"
+
+namespace cossb {
+
+ipc::ipc() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ipc::~ipc() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace cossb */
---
+++ include/util/ipc.hpp
@@ -0,0 +1,20 @@
+/**
+ * @brief	Internal Process Communication
+ */
+
+#ifndef _COSSB_UTIL_IPC_HPP_
+#define _COSSB_UTIL_IPC_HPP_
+
+namespace cossb {
+namespace base {
+
+class ipc {
+public:
+	ipc();
+	virtual ~ipc();
+};
+
+} /* namespace base */
+} /* namespace cossb */
+
+#endif /* _COSSB_UTIL_IPC_HPP_ */
