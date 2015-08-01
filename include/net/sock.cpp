d5b1dadce8eadde43030fd5d2401948f68c08711
elecun <elecun2@gmail.com>
2015-08-01 21:52:59 +0900

    first commit, socket wrapper

---
+++ include/net/sock.cpp
@@ -0,0 +1,21 @@
+/*
+ * sock.cpp
+ *
+ *  Created on: 2015. 8. 1.
+ *      Author: hwang-linux
+ */
+
+#include "sock.hpp"
+
+namespace cossb {
+
+sock::sock() {
+	// TODO Auto-generated constructor stub
+
+}
+
+sock::~sock() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace cossb */
---
+++ include/net/sock.hpp
@@ -0,0 +1,20 @@
+/**
+ * @brief	basic socket wrapper
+ */
+
+#ifndef _COSSB_NET_SOCK_HPP_
+#define _COSSB_NET_SOCK_HPP_
+
+namespace cossb {
+namespace com {
+
+class sock {
+public:
+	sock();
+	virtual ~sock();
+};
+
+} /* namespace communication */
+} /* namespace cossb */
+
+#endif /* _COSSB_NET_SOCK_HPP_ */
