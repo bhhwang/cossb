15a9a13f84ac59f6186b8d6f1f30ca884a0bbbc1
elecun <elecun2@gmail.com>
2015-08-02 18:42:31 +0900

    test application for service test

---
+++ utils/test/servicetest.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file		servicetest.cpp
+ * @brief		COSSB Service Test (on Linux)
+ * @author		Byunghun Hwang<bhhwang@nsynapse.com>
+ * @date 		2015. 8. 2
+ * @details	COSSB service test on Linux machine(embedded or not)
+ */
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
+
