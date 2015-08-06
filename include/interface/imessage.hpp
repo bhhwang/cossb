821a1368277226a7288c5a7bb699bde377dbc867
elecun <elecun2@gmail.com>
2015-08-06 20:50:21 +0900

    first commit, message interface class

---
+++ include/interface/imessage.hpp
@@ -0,0 +1,25 @@
+/**
+ * @file		imessage.hpp
+ * @brief		Message Interface class
+ * @author		Byunghun Hwang<bhhwang@nsynapse.com>
+ * @date 		2015. 8. 6
+ * @details	component message interface class
+ */
+#ifndef _COSSB_INTERFACE_IMESSAGE_HPP_
+#define _COSSB_INTERFACE_IMESSAGE_HPP_
+
+
+namespace cossb {
+namespace interface {
+
+
+class imessage {
+public:
+	virtual ~imessage() { }
+
+};
+} /* namespace interface */
+} /* namespace cossb */
+
+
+#endif /* _COSSB_INTERFACE_IMESSAGE_HPP_ */
