a6f6918261a5654d59f70c933d93f00fe84cef23
elecun <elecun2@gmail.com>
2015-08-06 20:59:28 +0900

    edit functions

--- include/instance.cpp
+++ include/instance.cpp
@@ -4,7 +4,6 @@
 #include "manager.hpp"
 #include "broker.hpp"
 #include "config.hpp"
-#include "configloader.hpp"
 
 #include <string>
 
@@ -13,47 +12,45 @@ using namespace std;
 namespace cossb {
 namespace core {
 
-bool cossb_init(base::config* config)
+bool cossb_init(const char* manifest)
 {
-	//1. create instances for system base
-	if(manager::system_manager::get()->setup(config))
-	{
-		cout << "cossb init success" << endl;
-	}
+	//1. load manifest file
+	if(!cossb_config->load(manifest))
+		return false;
 
-	//2. system authentication from server
-
-	//3. repository check
-
-	//4. access to local database
-
-	//5. start cossb engine
+	//2. create(setup) instances according to the manifest
+	if(!cossb_system_manager->setup(base::config::instance()))
+		return false;
 
 	return true;
 }
 
 void cossb_destroy()
 {
-	//1. destroy instances
+	base::config::destroy();
 	manager::system_manager::destroy();
-
-	//1. disconnect to servers and databases
-
-	//2. destroy all instances
-	//arch::singleton<manager::system_manager>::get()->destroy();
-	//manager::component_manager::get()->destroy();
-	//broker::component_broker::get()->destroy();
 }
 
 bool cossb_sync()
 {
-	//1. check accessibility repository
-
-	//2. transfer information to use services
-
-	//3. clean and update all service components
-
 	return true;
+}
+
+
+bool cossb_start()
+{
+	if(cossb_component_manager->run()==types::returntype::SUCCESS)
+		return true;
+
+	return false;
+}
+
+bool cossb_stop()
+{
+	if(cossb_component_manager->stop()==types::returntype::SUCCESS)
+		return true;
+
+	return false;
 }
 
 
