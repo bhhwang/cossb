e982b55e074af947ee35ef93668b68e54d9d641d
bhhwang <bhhwang@nsynapse.com>
2015-08-07 23:31:30 +0900

    interface headers

---
+++ include/interface.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file		interface.hpp
+ * @brief		interface headers
+ * @author		Byunghun Hwang<bhhwang@nsynapse.com>
+ * @date 		2015. 8. 7
+ * @details		COSSB interface headers
+ */
+
+#ifndef _COSSB_INTERFACE_HPP_
+#define _COSSB_INTERFACE_HPP_
+
+#if defined(__unix__) || defined(__gnu_linux__) || defined(linux) || defined(__linux__)
+
+#include "interface/icomponent.hpp"
+#include "interface/iauth.hpp"
+#include "interface/imessage.hpp"
+#include "interface/iprofile.hpp"
+#include "interface/isql.hpp"
+#include "interface/ilog.hpp"
+#include "interface/icomm.hpp"
+
+#endif
+
+#endif /* _COSSB_INTERFACE_HPP_ */
