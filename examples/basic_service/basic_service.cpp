849deddd44eb93f7c2bec97a534f7f5700401cd9
elecun <elecun2@gmail.com>
2015-08-08 17:35:15 +0900

    add print out code

--- examples/basic_service/basic_service.cpp
+++ examples/basic_service/basic_service.cpp
@@ -1,4 +1,7 @@
 #include "basic_service.hpp"
+#include <iostream>
+
+using namespace std;
 
 COMPONENT_INSTANCE(basic_service)
 COMPONENT_CREATE(basic_service)
@@ -7,26 +10,29 @@ COMPONENT_DESTROY
 basic_service::basic_service()
 :icomponent(COMPONENT(basic_service))
 {
-
+	cout << "create basic service instance" << endl;
 }
 basic_service::~basic_service()
 {
-
+	cout << "destroy basic service instance" << endl;
 }
 
 bool basic_service::setup()
 {
+	cout << "basic service call : setup" << endl;
 	return false;
 }
 bool basic_service::run()
 {
+	cout << "basic service call : run" << endl;
 	return false;
 }
 bool basic_service::stop()
 {
+	cout << "basic service call : stop" << endl;
 	return false;
 }
 void basic_service::request(imessage* msg)
 {
-
+	cout << "basic service call : request" << endl;
 }
