a51ab5294c46b2c86539e184b6eaf9ef9a325792
elecun <elecun2@gmail.com>
2015-08-08 16:09:39 +0900

    first commit, add basic service header file

---
+++ examples/basic_service/basic_service.hpp
@@ -0,0 +1,33 @@
+/**
+ * @file	basic_service.hpp
+ * @brief	simple service component
+ * @author	Byunghun Hwang<bhhwang@nsynapse.com>
+ * @date 	2015. 8. 7
+ * @details	console print component
+ */
+
+
+#ifndef _COSSB_EXAMPLE_BASIC_SERVICE_HPP_
+#define _COSSB_EXAMPLE_BASIC_SERVICE_HPP_
+
+#include "../../include/interface.hpp"
+
+using namespace cossb::interface;
+
+class basic_service : public icomponent
+{
+public:
+	basic_service();
+	virtual ~basic_service();
+
+	bool setup();
+	bool run();
+	bool stop();
+	void request(imessage* msg);
+
+};
+
+
+
+
+#endif /* _COSSB_EXAMPLE_BASIC_SERVICE_HPP_ */
