7f68a85e3b32a96dd25dc5b69a0110ec33f0b6bb
elecun <elecun2@gmail.com>
2015-08-14 00:21:28 +0900

    use logger which comes from a shared library

--- include/sysmanager.cpp
+++ include/sysmanager.cpp
@@ -8,6 +8,7 @@
 
 #include "manager.hpp"
 #include "logger.hpp"
+#include "soloader.hpp"
 
 namespace cossb {
 namespace manager {
@@ -20,11 +21,23 @@ system_manager::system_manager()
 system_manager::~system_manager()
 {
 	cossb_component_manager->destroy();
+	cossb_log->destroy();
 }
 
 bool system_manager::setup(base::config* config)
 {
-	//1. load dependent components
+
+	//1. load log library
+	for(auto dep:*config->get_library()) {
+		if(!dep->use.compare("log")) {
+			cout << "load logger" << endl;
+			cossb_log->_logger = base::soloader<interface::ilog>(dep->sofile.c_str()).load();
+			cossb_log->log("this is test");
+
+		}
+	}
+
+	//2. load dependent components
 	for(auto dep:*config->get_dependency()) {
 		if(dep->type==base::dependencyType::COMPONENT)
 			cossb_component_manager->install(dep->name.c_str());
