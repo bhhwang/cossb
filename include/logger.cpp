343e38a36c458080ef0ad28fed744b2aa6784a4f
elecun <elecun2@gmail.com>
2015-08-20 12:31:38 +0900

    remove comment

--- include/logger.cpp
+++ include/logger.cpp
@@ -64,43 +64,5 @@ void logger::adopt(interface::ilog* intlog)
 	_logger = intlog;
 }
 
-/*
-void logger::trace(const char* logstr)
-{
-	cout << _time.current_formatted() << "[TRACE] " << logstr << endl;
-}
-void logger::debug(const char* logstr)
-{
-	cout << _time.current_formatted() << "[DEBUG] " << logstr << endl;
-}
-void logger::info(const char* logstr)
-{
-	cout << _time.current_formatted() << "[INFO] " << logstr << endl;
-}
-void logger::notice(const char* logstr)
-{
-	cout << _time.current_formatted() << "[NOTICE] " << logstr << endl;
-}
-void logger::warn(const char* logstr)
-{
-	cout << _time.current_formatted() << "[WARNING] " << logstr << endl;
-}
-void logger::error(const char* logstr)
-{
-	cout << _time.current_formatted() << "[RROR] " << logstr << endl;
-}
-void logger::critical(const char* logstr)
-{
-	cout << _time.current_formatted() << "[CRITICAL] " << logstr << endl;
-}
-void logger::alert(const char* logstr)
-{
-	cout << _time.current_formatted() << "[ALERT] " << logstr << endl;
-}
-void logger::emerg(const char* logstr)
-{
-	cout << _time.current_formatted() << "[EMERGENCY] " << logstr << endl;
-}
-*/
 } /* namespace log */
 } /* namespace cossb */
