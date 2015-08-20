a92423456dbc8713df06cffad99808230dabc533
elecun <elecun2@gmail.com>
2015-08-20 22:11:36 +0900

    first commit, add for network service interface

---
+++ include/interface/inetservice.hpp
@@ -0,0 +1,26 @@
+/**
+ * @file		inetservice.hpp
+ * @brief		Network service interface
+ * @author		Byunghun Hwang<bhhwang@nsynapse.com>
+ * @date 		2015. 8. 20
+ * @details	Network interface service
+ */
+#ifndef _COSSB_INTERFACE_INETSERVICE_HPP_
+#define _COSSB_INTERFACE_INETSERVICE_HPP_
+
+namespace cossb {
+namespace interface {
+
+class inetservice {
+public:
+	inetservice() { }
+	virtual ~inetservice() { }
+
+};
+
+} /* namespace interface */
+} /* namespace cossb */
+
+
+
+#endif /* _COSSB_INTERFACE_INETSERVICE_HPP_ */
