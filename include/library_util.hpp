585db13fe38947e78563e5aae17cddda2654b996
elecun <elecun2@gmail.com>
2015-08-22 23:40:31 +0900

    .

---
+++ include/library_util.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file		library_util.hpp
+ * @brief		COSSB library utility
+ * @author		Byunghun Hwang<bhhwang@nsynapse.com>
+ * @date 		2015. 8. 13
+ * @details	library requires utilities
+ */
+
+#ifndef _COSSB_LIBRARY_UTIL_HPP_
+#define _COSSB_LIBRARY_UTIL_HPP_
+
+
+#if defined(__unix__) || defined(__gnu_linux__) || defined(linux) || defined(__linux__)
+
+
+#include "logger.hpp"	//for base logger
+
+#endif
+
+
+#endif /* _COSSB_LIBRARY_UTIL_HPP_ */
