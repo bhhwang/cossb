018d5f03dfdaced03a3e0d42250e4237c033e732
elecun <elecun2@gmail.com>
2015-08-22 23:38:03 +0900

    add endpoint for ipv4 and ipv6

--- include/net/endpoint.hpp
+++ include/net/endpoint.hpp
@@ -10,20 +10,43 @@
 #define _COSSB_NET_ENDPOINT_HPP_
 
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <cstring>
 
 namespace cossb {
 namespace net {
 
-struct endpoint : protected sockaddr_in {
+/**
+ * @brief	endpoint for IPv4
+ */
+struct endpoint_v4 : public sockaddr_in {
 public:
-	endpoint();
-	endpoint(unsigned int port);
-	endpoint(const char* ipaddr);
-	endpoint(struct sockaddr_in);
+	endpoint_v4() {
+		memset(ipv4, 0x00, sizeof(ipv4));
+	}
+	endpoint_v4(const char* ipaddr){
+		memset(ipv4, 0x00, sizeof(ipv4));
+		inet_pton(AF_INET, ipaddr, &(this->sin_addr));
+	}
 
 private:
-	char ip[16];
-	unsigned int port;
+	char ipv4[INET_ADDRSTRLEN];
+};
+
+/**
+ * @brief	endpoint for IPv6
+ */
+struct endpoint_v6 : public sockaddr_in6 {
+public:
+	endpoint_v6() {
+		memset(ipv6, 0x00, sizeof(ipv6));
+	}
+	endpoint_v6(const char* ipaddr) {
+		memset(ipv6, 0x00, sizeof(ipv6));
+		inet_pton(AF_INET6, ipaddr, &(this->sin6_addr));
+	}
+private:
+	char ipv6[INET6_ADDRSTRLEN];
 };
 
 } /* namespace net */
