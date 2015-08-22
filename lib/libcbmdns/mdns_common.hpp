a9b9d028e1121c6bf259c0d1b19db21daa8b1843
elecun <elecun2@gmail.com>
2015-08-22 23:39:56 +0900

    update mdns common, but not completed

--- lib/libcbmdns/mdns_common.hpp
+++ lib/libcbmdns/mdns_common.hpp
@@ -11,13 +11,25 @@
 
 namespace cossb {
 
+/**
+ * @brief	mDNS flags
+ */
+//QR (1 bit) - Query(0), Response(1)
+//OPCODE (4 bits) - kind of query
+//AA (1 bit) - Authoritative Answer
+//TC (1 bit) - TrunCation
+//RD (1 bit) - Recursion Desired : this bit directs the name server to pursue the query recursively
+//RA (1 bit) - Recursion Available
+//Z (3 bits) - Reserved for future use (must be set 0)
+//RCODE (4 bits) - Response code : 0(No error), 1(Format error), 2(Server failure), 3(Name Error), 4(Not implemented), 5(Refused)
+
 typedef struct _mDNSHeader {
 	unsigned short	id;		//transaction id
 	unsigned short	flags;
-	unsigned short	qdcount;
-	unsigned short	ancount;
-	unsigned short	nscount;
-	unsigned short	arcount;
+	unsigned short	qdcount;	//number of entries in the question section
+	unsigned short	ancount;	//number of resource records in the answer section
+	unsigned short	nscount;	//number of resource records in the authority section
+	unsigned short	arcount;	//number of resource records in the additional records section
 
 } mDNSHeader;
 
@@ -25,9 +37,19 @@ typedef struct _mDNSData {
 
 } mDNSData;
 
+typedef struct _mDNSQuestion {
+
+} mDNSQuestion;
+
+typedef struct _mDNSAnswer {
+
+} mDNSAnswer;
+
 typedef struct _mDNSFrame {
-	mDNSHeader	header;
-	mDNSData	data;
+	mDNSHeader		header;
+	mDNSQuestion	question;
+	mDNSAnswer		answer;
+	mDNSData		data;
 };
 
 } /* namespace cossb */
