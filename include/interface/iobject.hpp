3a006ad35476b7b6b3f1ec7d11df11d83d72e1a5
elecun <elecun2@gmail.com>
2015-08-22 19:43:04 +0900

    first commit, object interface class, there is no interface function to act something

---
+++ include/interface/iobject.hpp
@@ -0,0 +1,25 @@
+/**
+ * @file		iobject.hpp
+ * @brief		object interface
+ * @author		Byunghun Hwang<bhhwang@nsynapse.com>
+ * @date 		2015. 8. 22
+ * @details	object interface
+ */
+
+#ifndef _COSSB_INTERFACE_IOBJECT_HPP_
+#define _COSSB_INTERFACE_IOBJECT_HPP_
+
+
+namespace cossb {
+namespace interface {
+
+class iobject {
+public:
+	iobject() { }
+	virtual ~iobject() { }
+};
+}
+}
+
+
+#endif /* _COSSB_INTERFACE_IOBJECT_HPP_ */
