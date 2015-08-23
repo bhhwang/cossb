aa6a67ffe8869c8449265523f834ba6bd73b9c0a
elecun <elecun2@gmail.com>
2015-08-23 13:38:36 +0900

    first commit, cossb exception header

---
+++ include/exception.hpp
@@ -0,0 +1,35 @@
+/**
+ * @file		exception.hpp
+ * @brief		COSSB exception
+ * @author		Byunghun Hwang<bhhwang@nsynapse.com>
+ * @date 		2015. 8. 23
+ * @details	cossb exception header
+ */
+
+
+#ifndef _COSSB_EXCEPTION_HPP_
+#define _COSSB_EXCEPTION_HPP_
+
+
+#include <string>
+#include <exception>
+
+using namespace std;
+
+namespace cossb {
+
+struct cossb_exception : public exception {
+	cossb_exception(const char* ex):str_exception(ex) { }
+	~cossb_exception() throw() {}
+	const char* what() const throw() {
+		return str_exception.c_str();
+	}
+private:
+	string str_exception;
+
+};
+
+} /* namespace cossb */
+
+
+#endif /* _COSSB_EXCEPTION_HPP_ */
