6b4f3129928fd2185dbf89b114be40c813e8c92c
elecun <elecun2@gmail.com>
2015-08-24 18:59:11 +0900

    update constructor

--- include/net/udp.hpp
+++ include/net/udp.hpp
@@ -1,8 +1,9 @@
-/*
- * udp.hpp
- *
- *  Created on: 2015. 8. 22.
- *      Author: hwang-linux
+/**
+ * @file		udp.hpp
+ * @brief		COSSB UDP Library
+ * @author		Byunghun Hwang<bhhwang@nsynapse.com>
+ * @date 		2015. 8. 24
+ * @details	UDP Socket library
  */
 
 #ifndef _COSSB_NET_UDP_HPP_
@@ -15,7 +16,14 @@ namespace net {
 
 class udp : public sock {
 public:
-	udp(netType type):sock(type) { }
+	udp(netType type, net::endpoint_v4 address, int port):sock(type) {
+		switch(type)
+		{
+		case netType::CLIENT: break;
+		case netType::SERVER: break;
+		}
+	}
+	udp(netType type, int port):sock(type) { }
 	virtual ~udp() { }
 
 	int open(int port) {
