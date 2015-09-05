cae84db8a907123842033dca7532ddbb92c5d2da
elecun <elecun2@gmail.com>
2015-09-05 19:42:38 +0900

    insert log into the every function

--- examples/helloworld/helloworld.cpp
+++ examples/helloworld/helloworld.cpp
@@ -20,17 +20,20 @@ hello_world::~hello_world()
 
 bool hello_world::setup()
 {
+	cossb_log->log(cossb::log::loglevel::INFO, "called hello world setup");
 	return true;
 }
 bool hello_world::run()
 {
+	cossb_log->log(cossb::log::loglevel::INFO, "called hello world run");
 	return true;
 }
 bool hello_world::stop()
 {
+	cossb_log->log(cossb::log::loglevel::INFO, "called hello world stop");
 	return true;
 }
 void hello_world::request(cossb::interface::imessage* msg)
 {
-
+	cossb_log->log(cossb::log::loglevel::INFO, "called hello world request");
 }
