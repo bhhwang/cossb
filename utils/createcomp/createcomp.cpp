a5626525149aeac04d03947a4255989bd42af0dd
elecun <elecun2@gmail.com>
2015-09-06 17:18:04 +0900

    add create compnent utility

---
+++ utils/createcomp/createcomp.cpp
@@ -0,0 +1,21 @@
+/*
+ * createcomp.cpp
+ *
+ *  Created on: 2015. 9. 6.
+ *      Author: hwang-linux
+ */
+
+#include "createcomp.hpp"
+
+namespace cossb {
+
+createcomp::createcomp() {
+	// TODO Auto-generated constructor stub
+
+}
+
+createcomp::~createcomp() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace cossb */
---
+++ utils/createcomp/createcomp.hpp
@@ -0,0 +1,21 @@
+/*
+ * createcomp.hpp
+ *
+ *  Created on: 2015. 9. 6.
+ *      Author: hwang-linux
+ */
+
+#ifndef UTILS_CREATECOMP_CREATECOMP_HPP_
+#define UTILS_CREATECOMP_CREATECOMP_HPP_
+
+namespace cossb {
+
+class createcomp {
+public:
+	createcomp();
+	virtual ~createcomp();
+};
+
+} /* namespace cossb */
+
+#endif /* UTILS_CREATECOMP_CREATECOMP_HPP_ */
