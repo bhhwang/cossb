ae50e9226ff858286b94ddd59b6330ca5e732504
bhhwang <bhhwang@nsynapse.com>
2015-09-29 23:55:58 +0900

    first commit, http server library

---
+++ support/comphttpserver/libhttpserver.cpp
@@ -0,0 +1,18 @@
+/*
+ * libhttpserver.cpp
+ *
+ *  Created on: 2015. 9. 29.
+ *      Author: hwang
+ */
+
+#include "libhttpserver.hpp"
+
+libhttpserver::libhttpserver() {
+	// TODO Auto-generated constructor stub
+
+}
+
+libhttpserver::~libhttpserver() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ support/comphttpserver/libhttpserver.hpp
@@ -0,0 +1,17 @@
+/*
+ * libhttpserver.hpp
+ *
+ *  Created on: 2015. 9. 29.
+ *      Author: hwang
+ */
+
+#ifndef SUPPORT_COMPHTTPSERVER_LIBHTTPSERVER_HPP_
+#define SUPPORT_COMPHTTPSERVER_LIBHTTPSERVER_HPP_
+
+class libhttpserver {
+public:
+	libhttpserver();
+	virtual ~libhttpserver();
+};
+
+#endif /* SUPPORT_COMPHTTPSERVER_LIBHTTPSERVER_HPP_ */
