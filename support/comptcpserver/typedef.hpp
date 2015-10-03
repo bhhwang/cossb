47bb1e238b5fc799cb9adf9bb73c074a6654a2c7
hwang <hwang@hwanglaptop>
2015-10-03 22:29:27 +0900

    add type definition

---
+++ support/comptcpserver/typedef.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file		typedef.hpp
+ * @brief		TCP Server type definitions
+ * @author		Byunghun Hwang<bhhwang@nsynapse.com>
+ * @date 		2015. 10. 3
+ * @details	TCP 서버 타입 정의
+ */
+
+#ifndef _TCP_TYPEDEF_HPP
+#define _TCP_TYPEDEF_HPP
+
+#include <string>
+
+using namespace std;
+
+typedef std::string session_uuid;
+
+#endif
