69f92dd14f990452d786106e870c575c267a6362
hwang <hwang@hwanglaptop>
2015-10-04 22:41:42 +0900

    firmware updated for cc3200

--- devices/cc3200_launchpad/example_energia/example_energia.ino
+++ devices/cc3200_launchpad/example_energia/example_energia.ino
@@ -9,29 +9,39 @@ If you have any questions for this sample code, please contact us(bhhwang@nsynap
 #include <WiFiServer.h>
 #include <WiFiClient.h>
 
-char ssid[] = "hellfire";
-char password[] = "elec6887";
-WiFiClient client = 0;
-WiFiServer server(8000);
+volatile int yellow_state = LOW;
+volatile int red_state = LOW;
+volatile int green_state = LOW;
 
-boolean bConnected = false;
+char ssid[] = "nsynapseap";
+char password[] = "elec6887";
+WiFiClient client;
+WiFiServer server(8000);
 
 void setup()
 {
+  pinMode(PUSH1, INPUT_PULLUP);
+  pinMode(PUSH2, INPUT_PULLUP);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(YELLOW_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(YELLOW_LED, LOW);
+  digitalWrite(RED_LED, LOW);
+  
   Serial.begin(115200);
   WiFi.begin(ssid, password);
   
   while(WiFi.status()!=WL_CONNECTED) {
-    Serial.print(".");
+    net_led();
     delay(300);
   }
   
-  Serial.println("connected.");
-  Serial.println("Getting Local IP Address");
   while(WiFi.localIP() == INADDR_NONE) {
-    Serial.print(".");
+    net_led();
     delay(300);
   }
+  digitalWrite(GREEN_LED, HIGH);
   
   printWifiStatus();
   server.begin();
@@ -40,22 +50,64 @@ void setup()
 
 void loop()
 {
-  WiFiClient client = server.available();
-
-  if(client) {
-    if(!bConnected) {
-      bConnected = true;
-      client.flush();
-      client.println("hello, client!!");
-      Serial.println("client connected");
-    }
+  int i=0;
+  client = server.available();
+  
+  int push1_state = LOW;
+  int prev_push1_state = LOW;
+  int push2_state = LOW;
+  int prev_push2_state = LOW;
+  
+  unsigned char btn = 0x00;
+  unsigned char btn_value = 0x00;
+  
+  connet_led();
+  
+  if(client) 
+  {
+    Serial.println("New client is connected.");
+    //send(btn, btn_value);
     
-    if(client.available() > 0) {
-      char recv = client.read();
-      server.write(recv);
-      Serial.write(recv);
+    while(client.connected()) 
+    {
+      digitalWrite(YELLOW_LED, HIGH);
+      
+      push1_state = digitalRead(PUSH1);
+      push2_state = digitalRead(PUSH2);
+      
+      if(push1_state!=prev_push1_state) {
+        if(push1_state==HIGH) {
+          Serial.println("Push1 High");
+          send(PUSH1, HIGH);
+        }
+        else if(push1_state==LOW) {
+          Serial.println("Push1 Low");
+          send(PUSH1, LOW);
+        }
+      }
+      
+      if(push2_state!=prev_push2_state) 
+      {
+        if(push2_state==HIGH) {
+          Serial.println("Push2 High");
+          send(PUSH2, HIGH);
+        }
+        else if(push2_state==LOW) {
+          Serial.println("Push2 Low");
+          send(PUSH2, LOW);
+        }
+      }
+      
+      prev_push1_state = push1_state;
+      prev_push2_state = push2_state;
+      
+      delay(10);
     }
+    Serial.println("Client disconnected");
+    client.stop();
   }
+  
+  delay(300);
 }
 
 
@@ -78,3 +130,38 @@ void printWifiStatus() {
 
 }
 
+boolean endsWith(char* inString, char* compString) {
+  int compLength = strlen(compString);
+  int strLength = strlen(inString);
+  
+  //compare the last "compLength" values of the inString
+  int i;
+  for (i = 0; i < compLength; i++) {
+    char a = inString[(strLength - 1) - i];
+    char b = compString[(compLength - 1) - i];
+    if (a != b) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void net_led()
+{
+  green_state = !green_state;
+  digitalWrite(GREEN_LED, green_state);
+}
+
+void connet_led()
+{
+  yellow_state = !yellow_state;
+  digitalWrite(YELLOW_LED, yellow_state);
+}
+
+
+void send(unsigned char b, unsigned char v) {
+  if(client.connected()) {
+    unsigned char data[12] = {'/','S','T','A','R','T',b,v, '/','E','N','D'};
+    client.write(data, sizeof(data));
+  }
+}
