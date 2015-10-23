41ae57d6ac7c193453dc2b90b1831847a43d57a9
bhhwang <bhhwang@nsynapse.com>
2015-10-23 20:33:58 +0900

    change component interface macro simply

--- examples/wavealarm/wavealarm.cpp
+++ examples/wavealarm/wavealarm.cpp
@@ -10,9 +10,11 @@
 #include <SFML/Audio.hpp>	//depends on SFML library
 
 
-COMPONENT_INSTANCE(wavealarm)
-COMPONENT_CREATE(wavealarm)
-COMPONENT_DESTROY
+//COMPONENT_INSTANCE(wavealarm)
+//COMPONENT_CREATE(wavealarm)
+//COMPONENT_DESTROY
+
+USE_COMPONENT_INTERFACE(wavealarm)
 
 
 wavealarm::wavealarm()
