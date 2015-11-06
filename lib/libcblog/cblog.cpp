f3d04761088dca58fd3a0ae633c83a5934d2f48f
bhhwang <hwang@hwanglaptop>
2015-11-06 11:19:02 +0900

    notice level was colored (green)

--- lib/libcblog/cblog.cpp
+++ lib/libcblog/cblog.cpp
@@ -24,7 +24,7 @@ void cblog::log(const log::loglevel& level, const char* logstr)
 	case	log::loglevel::TRACE:	cout << _time.current_formatted() << "[TRACE] " << logstr << endl;	break;
 	case	log::loglevel::DEBUG:	cout << _time.current_formatted() << "[DEBUG] " << logstr << endl;	break;
 	case	log::loglevel::INFO:		cout  << _time.current_formatted() << "[INFO] " << logstr << endl;	break;
-	case	log::loglevel::NOTICE:  cout << _time.current_formatted() << "[NOTICE] " << logstr << endl;	break;
+	case	log::loglevel::NOTICE:  cout << "\033[1;32m" << _time.current_formatted() << "[NOTICE] " << logstr << "\033[0m" << endl;	break;
 	case	log::loglevel::WARN: 	cout << _time.current_formatted() << "[WARNING] " << logstr << endl;	break;
 	case	log::loglevel::ERROR: 	cout << "\033[1;31m" << _time.current_formatted() << "[ERROR] " << logstr << "\033[0m" << endl;	break;
 	case	log::loglevel::CRITICAL: cout << _time.current_formatted() << "[CRITICAL] " << logstr << endl;	break;
